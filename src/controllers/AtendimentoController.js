8e574a1aef6eb5a55cd082b741a08885c5c45a5b
Renata Mie <renata_mie@hotmail.com>
2021-06-08 14:29:18 -0300

    add tables

--- src/controllers/AtendimentoController.js
+++ src/controllers/AtendimentoController.js
@@ -6,7 +6,7 @@ module.exports = {
     async show(req,res) {
 
         const atendimento= await Atendimento.findAll({
-            where: {data_atendimento: {[Op.eq]: new Date()}},
+            where: {data_atendimento: {[Op.eq]: new Date().toDateString()}},
         }).then(res => res.id);
         return res.json(atendimento);
     },
