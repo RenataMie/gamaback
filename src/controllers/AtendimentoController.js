fb7739f6bfce020094367e01829c59e00ca27b46
Renata Mie <renata_mie@hotmail.com>
2021-06-08 17:48:51 -0300

    add tables

--- src/controllers/AtendimentoController.js
+++ src/controllers/AtendimentoController.js
@@ -8,9 +8,15 @@ module.exports = {
     async show(req,res) {
         const hoje = new Date().toISOString().slice(0,10)
 
-        const atendimento= await Atendimento.findAll({
-            where: { data_atendimento: {[Op.eq]: hoje}}
-        })
+        const atendimento= await Atendimento.findAll(
+            [
+                {where: { data_atendimento: {[Op.eq]: hoje}}, order: [ [ 'data_atendimento', 'DESC' ]] },
+
+                {include:[
+                    {association: "paciente_atendimento"},
+                    {association: "especialista_atendimento"}
+                ]}
+            ])
         return res.json(atendimento);
     },
 
